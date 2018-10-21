1e3fdf51375132196814f82553eb18cfaee4f19f
Nico Hawthorne <nahawtho@thor.soe.ucsc.edu>
2018-10-20 19:34:33 -0700

    can compile but not comply w RT

--- src/reuleaux.cc
+++ src/reuleaux.cc
@@ -1,10 +1,23 @@
 #include <vector>
 #include "reuleaux.h"
+#include "point.h"
+#include "containable.h"
 
 ReuleauxTriangle::ReuleauxTriangle(const Point vertices[3]) {
 
 	std::vector<Point> vecpts (vertices, vertices + sizeof(*vertices)
 							/ sizeof(Point));
 	
-	//const Point* pts = &vertices_[0];
+}
+
+bool ReuleauxTriangle::containedBy(Circle &circle){
+	throw "not implemented";
+}
+
+bool ReuleauxTriangle::containedBy(RegularConvexPolygon &polygon){
+	throw "not implemented";
+}
+
+bool ReuleauxTriangle::containedBy(ReuleauxTriangle &rt){
+	throw "not implemented";
 }
